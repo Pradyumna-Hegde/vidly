e6c6bd8ce9baad9e7525f6ccbb2eaf466042c083
Pradyumna-Hegde <pradyumna.coder@gmail.com>
2022-10-26 22:21:39 +0530

    fixed the bug in <Like /> component

--- src/components/common/Like.jsx
+++ src/components/common/Like.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Like = (onClick, liked) => {
+const Like = (props) => {
   let classes = "fa fa-heart";
-  if (!liked) classes += "-o";
+  if (!props.liked) classes += "-o";
   return (
     <i
-      onClick={onClick}
+      onClick={props.onClick}
       style={{ cursor: "pointer" }}
       className={classes}
       aria-hidden="true"
